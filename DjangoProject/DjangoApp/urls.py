e34e738e60e6180bc25be6525f41762adebd6b1d
Shubham Anand <shubhamanand148@gmail.com>
2022-12-22 00:09:38 +0530

    Word Counter

--- DjangoProject/DjangoApp/urls.py
+++ DjangoProject/DjangoApp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+
+    #This is the default/Home page of the website. It gets the look from template -> index.html
+    path('', views.index, name='index'),
+
+
+    path('count', views.count, name='count')
 ]
--- DjangoProject/DjangoApp/views.py
+++ DjangoProject/DjangoApp/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def count(request):
+    text = request.POST['text']
+    no_of_words = len(text.split())
+    return render(request, 'count.html', {'number': no_of_words})

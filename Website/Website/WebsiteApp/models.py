68f24c6a536a89858e6012dce3bb7f3bafe7c901
Shubham Anand <shubhamanand148@gmail.com>
2023-01-18 00:24:29 +0530

    Website Files

--- Website/Website/WebsiteApp/models.py
+++ Website/Website/WebsiteApp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    title: str
-    description: str
-    image: str
-    is_true: bool
+#To use a database we need to migrate all the data from models to db.sqlite3 database.
+#i.e. To update the database after making any change in the Models file run below commands.
+#To use the database, we need to add the App to the INSTALLED_APPS list in Settings.py file.
+#python manage.py makemigrations
+#python manage.py migrate
+
+#Then to see the database in admin page (http://127.0.0.1:8000/admin), register the database in admin file.
+
+class Feature(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
--- Website/Website/WebsiteApp/views.py
+++ Website/Website/WebsiteApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -15,6 +17,28 @@ def index(request):
     feature2.title = "Affordable"
     feature2.description = "We are affordable."
     feature2.image = "{% static 'images/time-seat-icon.png' %}"
+    
 
-    features = [feature1, feature2]
-    return render(request, 'index.html', {'feature': feature1})
+    #features = [feature1, feature2]
+    return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2 })
+
+#The below block allows the users to call the register.html file. And then users can create account/register
+def register(request):
+    if request.method == 'Post':
+        username = request.Post['username']
+        email = request.Post['email id']
+        password = request.Post['password']
+        password2 = request.Post['password2']
+
+        if password == password2:
+            #Check if the user is already registered and redirect him to the register screen with the message.
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already registered previously.')
+                return redirect('register')
+
+            #Check if the username is already used.
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already user. Please use another username.')
+                return redirect('register')
+
+        return render(request, 'register.html')
